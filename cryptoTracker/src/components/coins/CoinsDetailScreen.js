026934e0dc74381482e5a0b724eefc184ac08899
Andres Betancurt <abetancurt@tecmovin.com>
2021-03-04 18:05:55 -0500

    Adding second screen

---
+++ cryptoTracker/src/components/coins/CoinsDetailScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CoinsDetailScreen = () => {
+
+    return (
+        <View>
+            <Text>
+                Detail Coins
+            </Text>
+        </View>
+    );
+};
+
+export default CoinsDetailScreen;
--- cryptoTracker/src/components/coins/CoinsScreen.js
+++ cryptoTracker/src/components/coins/CoinsScreen.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
-const CoinsScreen = () => {
+const CoinsScreen = (props) => {
+
+    const handlePress = () => {
+        console.log("go to detail", props);
+        props.navigation.navigate('CoinDetail');
+    };
+
     return (
-        <View>
-            <Text>
+        <View style={styles.container}>
+            <Text style={styles.titleText}>
                 Screen Coins
             </Text>
+            <Pressable
+                onPress={handlePress}
+                style={styles.btn}
+            >
+                <Text style={styles.btnText}>
+                    Ir a Detail
+                </Text>
+            </Pressable>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "red"
+    },
+    titleText: {
+        color: "#fff",
+        textAlign: "center"
+    },
+    btn: {
+        padding: 8,
+        backgroundColor: "blue",
+        borderRadius: 8,
+        margin: 16
+    },
+    btnText: {
+        color: "#fff",
+        textAlign: "center"
+    }
+});
 
 export default CoinsScreen;
